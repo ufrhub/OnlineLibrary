e473075c8b2ff17cc733e1ac1517991306eff905
Umar Farooque <mdumarfarooqu@gmail.com>
2024-11-18 01:09:57 +0530

    Fixed Issues and Bugs

--- src/Components/BrowseBooksPage/BrowseBooks.js
+++ src/Components/BrowseBooksPage/BrowseBooks.js
@@ -6,6 +6,8 @@ import MainLoader from "../../HelperComponents/Loader/MainLoader";
 import BookCard from "../../HelperComponents/Cards/BookCard";
 import { useNavigate } from "react-router-dom";
 import BackButton from "../../Assets/back-button.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const BrowseBooks = () => {
     const [Books, setBooks] = useState([]);
@@ -14,6 +16,16 @@ const BrowseBooks = () => {
     const { BooksData, Error } = useFetchBooksData();
 
     const Navigate = useNavigate();
+
+    const Categories = [
+        "All", "Adventure", "Autobiography", "Biography", "Business and Economics",
+        "Children's Fiction", "Cookbooks", "Crime Fiction", "Drama", "Dystopian",
+        "Epistolary", "Fable", "Fairy Tale", "Fantasy", "Graphic Novels", "Health and Wellness",
+        "Historical Fiction", "History", "Horror", "Magical Realism", "Memoir",
+        "Mystery", "Philosophy", "Poetry", "Politics", "Religion and Spirituality",
+        "Romance", "Satire", "Science and Nature", "Science Fiction", "Self-Help",
+        "Thriller", "Travel", "True Crime", "Urban Fantasy", "Western", "Young Adult"
+    ];
 
     // Render the content
     useEffect(() => {
@@ -33,33 +45,54 @@ const BrowseBooks = () => {
 
     return (
         <React.Fragment>
-            <MainHeader />
-
-            <button className="back-btn" type="button" onClick={HandleOnBackPress}>
-                <img src={BackButton} alt="BackButton" />
-            </button>
-
-            <search className="search-section">
-
-            </search>
+            <MainHeader style={{ marginTop: "0px" }} />
 
             {
                 isLoading ?
                     <MainLoader />
                     :
                     <div className="browse-books">
-                        <section className="filter-section">
+                        <div className="top">
+                            <button className="back-btn" type="button" onClick={HandleOnBackPress}>
+                                <img src={BackButton} alt="BackButton" />
+                            </button>
 
-                        </section>
+                            <form className="form">
+                                <input type="text" name="searchText" placeholder="Type Book Title or Author" />
+                                <button type="button" className="search-btn"><FontAwesomeIcon icon={faSearch} bounce className="faSearch" />Search</button>
+                            </form>
+                        </div>
 
-                        <div className="books-container">
-                            {
-                                Books.map((Book, Index) => {
-                                    return (
-                                        <BookCard key={Index} Book={Book} />
-                                    )
-                                })
-                            }
+
+                        <div className="bottom">
+                            <section className="filter-section">
+                                <form>
+                                    <div className="segment">
+                                        <h1>Filter</h1>
+                                    </div>
+
+                                    <label>
+                                        <span>Select Category:</span>
+                                        <select>
+                                            {
+                                                Categories.map((category, index) => (
+                                                    <option key={index}>{category}</option>
+                                                ))
+                                            }
+                                        </select>
+                                    </label>
+                                </form>
+                            </section>
+
+                            <div className="books-container">
+                                {
+                                    Books.map((Book, Index) => {
+                                        return (
+                                            <BookCard key={Index} Book={Book} />
+                                        )
+                                    })
+                                }
+                            </div>
                         </div>
                     </div>
             }
--- src/HelperComponents/Header/MainHeader.js
+++ src/HelperComponents/Header/MainHeader.js
@@ -3,9 +3,9 @@ import "./Header.css";
 import Logo from "../../Assets/logo.png";
 import { Link } from "react-router-dom";
 
-const MainHeader = () => {
+const MainHeader = (Props) => {
     return (
-        <header>
+        <header style={Props.style}>
             <Link to={"/"} className="logo">
                 <img src={Logo} alt="Logo" />
             </Link>
